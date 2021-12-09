8c822969445f3f2dc93ed77aef07efc3e9cd1a3b
r79007 <rohan20537@iiitd.ac.in>
2021-12-09 16:29:27 +0530

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.ap_project_snakesandladders {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+    
+    opens com.example.ap_project_snakesandladders to javafx.fxml;
+    exports com.example.ap_project_snakesandladders;
+}
