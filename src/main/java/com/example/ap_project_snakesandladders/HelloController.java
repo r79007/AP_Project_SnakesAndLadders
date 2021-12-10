fa6e3700854400875e74a336988acc6d1320f828
Aishwary13 <aishwary20490@iiitd.ac.in>
2021-12-10 21:51:52 +0530

    dice_updated

--- src/main/java/com/example/ap_project_snakesandladders/HelloController.java
+++ src/main/java/com/example/ap_project_snakesandladders/HelloController.java
@@ -1,7 +1,14 @@
 package com.example.ap_project_snakesandladders;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.util.Random;
 
 public class HelloController {
     @FXML
@@ -10,5 +17,21 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private ImageView dice;
+
+    @FXML
+    private Button butt;
+
+    Random rand = new Random();
+
+    @FXML
+    void roll_button(ActionEvent event) throws InterruptedException{
+        int number = rand.nextInt(6)+1;
+        String path = "src/main/resources/dice"+number+".png";
+        File file = new File(path);
+        dice.setImage(new Image(file.toURI().toString()));
     }
 }
