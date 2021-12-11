15446dff7a306c97dc8b6ecdf39f1dab39b93a64
Aishwary13 <aishwary20490@iiitd.ac.in>
2021-12-11 10:46:11 +0530

    more_class_added

---
+++ src/main/java/com/example/ap_project_snakesandladders/Ladder.java
@@ -0,0 +1,40 @@
+package com.example.ap_project_snakesandladders;
+
+public class Ladder {
+    private int start_X;
+    private int start_y;
+    private int end_x;
+    private int end_y;
+
+    public int getStart_X() {
+        return start_X;
+    }
+
+    public void setStart_X(int start_X) {
+        this.start_X = start_X;
+    }
+
+    public int getStart_y() {
+        return start_y;
+    }
+
+    public void setStart_y(int start_y) {
+        this.start_y = start_y;
+    }
+
+    public int getEnd_x() {
+        return end_x;
+    }
+
+    public void setEnd_x(int end_x) {
+        this.end_x = end_x;
+    }
+
+    public int getEnd_y() {
+        return end_y;
+    }
+
+    public void setEnd_y(int end_y) {
+        this.end_y = end_y;
+    }
+}
---
+++ src/main/java/com/example/ap_project_snakesandladders/Player.java
@@ -0,0 +1,31 @@
+package com.example.ap_project_snakesandladders;
+
+public class Player {
+    private String name;
+    private int x_pos;
+    private int y_pos;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getX_pos() {
+        return x_pos;
+    }
+
+    public void setX_pos(int x_pos) {
+        this.x_pos = x_pos;
+    }
+
+    public int getY_pos() {
+        return y_pos;
+    }
+
+    public void setY_pos(int y_pos) {
+        this.y_pos = y_pos;
+    }
+}
